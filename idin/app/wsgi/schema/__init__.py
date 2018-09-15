48bd66421396f424b4ba3c9ec4b3c58cf9ba1cce
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-09-15 23:46:10 +0200

    qsa: Compile top-level HTTP endpoint schema package

--- idin/app/wsgi/schema/__init__.py
+++ idin/app/wsgi/schema/__init__.py
@@ -3,4 +3,5 @@ from .health import Health
 from .issuer import Issuer
 from .issuerset import IssuerSet
 from .servicenotavailable import ServiceNotAvailable
+from .transaction import Transaction
 from .version import Version
