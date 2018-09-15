f7386590a9fe6e4a1269a46afe60537f21300a32
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-09-15 22:33:50 +0200

    qsa: Compile endpoints from OpenAPI definition

--- idin/app/wsgi/endpoints/__init__.py
+++ idin/app/wsgi/endpoints/__init__.py
@@ -2,11 +2,13 @@ from werkzeug.routing import Map
 
 from .health import HealthEndpoint
 from .version import VersionEndpoint
+from .issuer import IssuerEndpoint
 
 
 urlpatterns = Map([
     HealthEndpoint.as_rule(),
     VersionEndpoint.as_rule(),
+    IssuerEndpoint.as_rule(),
 ])
 
 
