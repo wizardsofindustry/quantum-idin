3a5c01101c155a3d64a9436373001b2e6becf846
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-09-16 01:29:39 +0200

    qsa: Compile TransactionFinder

--- idin/infra/finder/transaction/base.py
+++ idin/infra/finder/transaction/base.py
@@ -5,10 +5,10 @@ from sq.readmodel import Finder
 class BaseTransactionFinder(Finder):
     session = ioc.class_property('DatabaseSessionFactory')
 
-    def reference(self):
+    def reference(self, txid):
         raise NotImplementedError("Subclasses must override this method.")
 
-    def result(self):
+    def result(self, txid, ec):
         raise NotImplementedError("Subclasses must override this method.")
 
 
