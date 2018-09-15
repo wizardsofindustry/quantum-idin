93f1d5054adfedaf579b7bb15c3ca006aee65f74
Cochise Ruhulessin <cochise.ruhulessin@wizardsofindustry.net>
2018-09-16 01:07:28 +0200

    Implement TransactionCtrl.patch()

--- idin/app/ctrl/transaction/impl.py
+++ idin/app/ctrl/transaction/impl.py
@@ -7,7 +7,16 @@ class TransactionCtrl(BaseTransactionCtrl):
     endpoint.
     """
 
+    async def patch(self, request, *args, **kwargs):
+        """Returns the results of the transaction identified by the request
+        payload.
+        """
+        return self.idin.result(request.payload)
+
     async def post(self, request, *args, **kwargs):
+        """Creates a new iDIN transaction with the parameters specified in the
+        request payload.
+        """
         return 201, self.idin.transaction(request.payload)
 
     async def get(self, request, *args, **kwargs):
